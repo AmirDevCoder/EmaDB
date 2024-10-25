6e29732580e3a1cd7a38c400097d846729fcabc4
emamagic <emamagic.dev@gmail.com>
2024-10-25 16:16:34 +0330

    update example

--- example/src/main/java/com/emamagic/Main.java
+++ example/src/main/java/com/emamagic/Main.java
@@ -4,18 +4,28 @@ import com.emamagic.entity.Log;
 import com.emamagic.entity.User;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(EmaDB.upsert(getUser()));
+        // Insert or update a user record
+        Optional<User> upsertedUser = EmaDB.upsert(getUser());
+
+        // Delete records
+        boolean isDeleted = EmaDB.delete(getUser());
+
+        // Read records
+        Optional<List<User>> users = EmaDB.read(User.class);
+
+        // Close the database connection
         EmaDB.close();
     }
 
     private static User getUser() {
         var user = new User();
-        user.setId(2);
-        user.setName("marr");
-        user.setEmail("mary@gmail.com");
+        user.setName("ali");
+        user.setEmail("ali@gmail.com");
         return user;
     }
 
